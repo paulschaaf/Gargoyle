506f31b1602b58ef5e4dfcf48f2460802de507b6
P.G. Schaaf <paul.schaaf@gmail.com>
2017-10-03 18:16:56 -0700

    Working on reading filesystem assets

---
+++ app/src/androidTest/java/com/github/paulschaaf/gargoyle/microtests/FilesystemLoaderTest.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2017 P.G. Schaaf <paul.schaaf@gmail.com>
+ * This file is part of Gargoyle.
+ * Gargoyle is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.paulschaaf.gargoyle.microtests
+
+import android.os.Environment
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class FilesystemLoaderTest {
+
+  @Test
+  fun verifyStoryFilesFound() {
+    println(Environment.getExternalStorageDirectory().listFiles())
+  }
+}
