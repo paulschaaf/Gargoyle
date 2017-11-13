d175f39f46b28e35dfb4ceb7bdc381d1a314423a
P.G. Schaaf <paul.schaaf@gmail.com>
2017-11-13 13:28:41 -0800

    Better error reporting in FestAssertionsEnhancement

--- app/src/main/java/com/github/paulschaaf/gargoyle/ContentValuesEnhancement.kt
+++ app/src/main/java/com/github/paulschaaf/gargoyle/ContentValuesEnhancement.kt
@@ -15,27 +15,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * Copyright © 2017 P.G. Schaaf <paul.schaaf@gmail.com>
- * This file is part of Gargoyle.
- * Gargoyle is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 import android.content.ContentValues
 
 @Suppress("UNCHECKED_CAST")
-fun <T> ContentValues.read(name: String, clazz: Class<T>): T = when (clazz) {
+fun <T> ContentValues.read(name: String, klass: Class<T>): T = when (klass) {
   Boolean::class.java          -> getAsBoolean(name)
   Byte::class.java             -> getAsByte(name)
   ByteArray::class.java        -> getAsByteArray(name)
